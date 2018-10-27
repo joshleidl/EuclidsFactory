a8e3f2eaa85d61b7dab0f841de036fa58ecf579b
joshleidl <joshleidl@gmail.com>
2018-10-27 09:57:01 -0600

    Initial commit

---
+++ Assets/Scripts/TouchRotatable.cs
@@ -0,0 +1,63 @@
+﻿/* Programmer: Josh Leidl
+ * Desc: This script enables the rotation of objects based on mouse clicks 90 degres at a time.
+ * Use: Left click rotates upon x axis, right click rotates upon y axis
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class TouchRotatable : MonoBehaviour
+{
+    bool can_rotate = true;
+    // Possible degrees: 
+    int goal_side_x = 0;
+    int angle_remaining = 0;
+    int delta_angle = 0;
+    bool rotate_on_x = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On LMB downpress
+        if (Input.GetMouseButtonDown(0) && can_rotate)
+        {
+            can_rotate = false;
+            rotate_on_x = true;
+            goal_side_x = goal_side_x > 3 ? goal_side_x + 1 : 0;
+
+            angle_remaining = 90;
+            delta_angle = 3;
+        }
+
+        // On RMB downpress
+        if (Input.GetMouseButtonDown(1) && can_rotate)
+        {
+            can_rotate = false;
+            rotate_on_x = false;
+
+            angle_remaining = 90;
+            delta_angle = 3;
+        }
+
+        if (!can_rotate)
+        {
+            if (rotate_on_x)
+                transform.Rotate(0, delta_angle, 0);
+            else
+                transform.Rotate(delta_angle, 0, 0);
+
+            angle_remaining -= delta_angle;
+            if (angle_remaining == 0) {
+                can_rotate = true;
+                delta_angle = 0;
+            }
+                
+        }
+    }
+}
